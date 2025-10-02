bfc1ec8588246c2afcc4055cd1686886c2a59536
Maxime Briand <pro.maximebriand@gmail.com>
2025-10-02 10:04:57 +0200

    ex00: Starting ex00

---
+++ mobileModule00/ex00/lib/ex00.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Ex00Page extends StatefulWidget {
+  const Ex00Page();
+
+  @override
+  State<Ex00Page> createState() => _Ex00PageState();
+}
+
+class _Ex00PageState extends State<Ex00Page> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text("Hello"),
+    );
+  }
+}
